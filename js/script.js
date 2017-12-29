563de1c64afd4ae6a36a5da675d9269f370c9c61
Alex Kissane <alexkissane@live.co.uk>
2017-12-29 23:44:53 +0000

    Added JS comments

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
 /* Google Maps API */
 function myMap() {
+  /* Define the variable with map value */
   var mapProp= {
       center:new google.maps.LatLng(51.508742,-0.120850),
       zoom:5,
   };
 
+  /* set the map to fill the googleMap ID (in this case it's a DIV) */
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
 /* Contact form Notification */
-if(document.getElementById("Submit")){
+
+/* Event listener checks if the button has been clicked */
   document.getElementById("Submit").addEventListener("click", alertContact);
-}
+
+/* Function shows an alert to the user once the button has been pressed */
 
 function alertContact() {
   window.alert("Thank you for getting in touch. We will be back with you shortly. (Usually 24-48 hours)");
